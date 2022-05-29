bb625c8f1e44e7a777268ee0be1d326ecb5d0e6f
timjancic <timjancic.gravity@gmail.com>
2022-05-29 15:29:09 -0500

    First components of making transition of showing more projects

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
 const moreProjectsContainer = document.getElementById("more-projects-container");
-const moreProjectsButton = document.getElementById("more-projects-b");
-const lessProjectsButton = document.getElementById("less-projects-b");
+const moreLessProjectsButton = document.getElementById("more-less-projects-b");
 
-moreProjectsButton.addEventListener('click', () => {
-  moreProjectsContainer.classList.remove("visually-hidden");
-  moreProjectsButton.classList.add("visually-hidden");
-});
 
-lessProjectsButton.addEventListener('click', () => {
-  moreProjectsContainer.classList.add("visually-hidden");
-  moreProjectsButton.classList.remove("visually-hidden");
+moreLessProjectsButton.addEventListener('click', () => {
+  if (moreLessProjectsButton.classList[1] == "btn-primary") {
+    moreProjectsContainer.classList.add("extra-show");
+
+    let finishAnimate = setTimeout(function() {
+      moreLessProjectsButton.textContent = "Show Less Projects";
+      moreLessProjectsButton.classList.add("btn-secondary");
+      moreLessProjectsButton.classList.remove("btn-primary");
+    },(800));
+  } else {
+    moreProjectsContainer.classList.remove("extra-show");
+
+    let finishAnimate = setTimeout(function() {
+      moreLessProjectsButton.textContent = "Show More Projects";
+      moreLessProjectsButton.classList.add("btn-primary");
+      moreLessProjectsButton.classList.remove("btn-secondary");
+    },(800));
+  }
 });
